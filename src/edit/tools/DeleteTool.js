4fe52b47f360f7735c193b158dcae9dd734f1dd9
KevTys <kevintannnnnn@gmail.com>
2025-03-16 15:23:31 +0800

    added touch events for delete tool

--- src/edit/tools/DeleteTool.js
+++ src/edit/tools/DeleteTool.js
@@ -9,14 +9,39 @@ export default function setDeleteTool() {
     Array.from(plane.children).forEach(line => {
         line.classList.add('selector')
         line.style.strokeWidth = backend.data.envVar.strokeWidth * 1.5
+        line.addEventListener('touchend', handleLineDelete)
     })
 
     return () => {
         Array.from(plane.children).forEach(line => {
             line.classList.remove('selector')
             line.style.strokeWidth = backend.data.envVar.strokeWidth
+            line.removeEventListener('touchend', handleLineDelete)
         })
         screen.style.display = 'block'
         backend.draw.drawPattern()
+    }
+}
+
+function handleLineDelete(e) {
+    e.preventDefault()
+    let lineElem
+
+    if (e.type === 'touchend') {
+        if (e.changedTouches && e.changedTouches[0]) {
+            const touch = e.changedTouches[0]
+            lineElem = document.elementFromPoint(touch.clientX, touch.clientY)
+        }
+    } else {
+        lineElem = e.target
+    }
+
+    if (lineElem && lineElem.tagName.toLowerCase() === 'line') {
+        let lineId = lineElem.id
+        if (lineId && backend.data.edgeObj[lineId]) {
+            backend.data.deleteEdgeSeg(lineId)
+            lineElem.remove()
+            backend.history.overwriteHistory()
+        }
     }
 }
